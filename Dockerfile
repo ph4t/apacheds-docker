9113f6ed06362e17407dc5749e334479b14b8a29
Svetozar Misljencevic <svetozar01@gmail.com>
2017-04-26 09:00:11 -0400

    properly terminate apacheds on docker stop

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
+#todo oracle jdk7 recommended at http://directory.apache.org/apacheds/basic-ug/1.3-installing-and-starting.html
 FROM java:7-alpine
-MAINTAINER Michael Riedmann @ https://www.github.com/mriedmann
+LABEL original-author="Michael Riedmann @ https://www.github.com/mriedmann" maintainer="https://github.com/ph4t/apacheds-docker"
 
 ENV APACHEDS_VERSION 2.0.0-M23
 ENV APACHEDS_MD5 1b380b7eace07e338578a66a4c625d61
 ENV APACHEDS_DATA /opt/apacheds/instances
 ENV APACHEDS_INSTANCE default
 ENV APACHEDS_USER apacheds
-ENV APACHEDS_GROUP apacheds
+ENV APACHEDS_GROUP apachedsdocker build
 
 RUN apk --no-cache add bash tar sudo
 
@@ -27,7 +28,9 @@ RUN adduser -S apacheds -h /opt/apacheds -H && \
     chmod u+rx /run.sh
 
 VOLUME /opt/apacheds/instances
-	
-EXPOSE 10389 10636 60088 60464 8080 8443
 
-CMD ["/run.sh"]
+EXPOSE 10389 10636
+
+#use entrypoint instead of CMD so that run.sh is PID1 which will receive SIGTERM. Otherwise 'docker stop' will always forcefully terminate the container
+#see https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86 & https://docs.docker.com/engine/reference/builder/#exec-form-entrypoint-example
+ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
+# x - print executed commands; e - exit if a program fails
+
+
+term_handler(){
+    # stop service and clean up here
+    echo "stopping apacheds"
+    sudo -u ${APACHEDS_USER} bash -c "/opt/apacheds/bin/apacheds.sh ${APACHEDS_INSTANCE} stop"
+    echo "apacheds stopped"
+}
+#trap signals and call the term handler
+trap "term_handler" SIGHUP SIGINT SIGQUIT SIGTERM
+
 
 if [ -n "$(ls -A ${APACHEDS_DATA})" ]
   then
@@ -15,5 +27,16 @@ chown -R ${APACHEDS_USER} /opt/apacheds/
 pidFile=${APACHEDS_DATA}/run/apacheds-${APACHEDS_INSTANCE}.pid
 [[ -e $pidFile ]] && rm $pidFile
 
-# Execute the server in console mode and not as a daemon.
-sudo -u ${APACHEDS_USER} bash -c "/opt/apacheds/bin/apacheds.sh ${APACHEDS_INSTANCE} run"
+# Always repair because the data will be corrupted in case of improper shutdown
+# sudo -u ${APACHEDS_USER} bash -c "/opt/apacheds/bin/apacheds.sh ${APACHEDS_INSTANCE} repair"
+
+
+# Execute the server in background. If executed in foreground the trap doesn't get called for some reason
+sudo -u ${APACHEDS_USER} bash -c "/opt/apacheds/bin/apacheds.sh ${APACHEDS_INSTANCE} start"
+
+
+#wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
